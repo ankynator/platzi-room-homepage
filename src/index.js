6ce267d6c990c0b5a5faedbbfaea70f9486b9620
ankynator <ankynator@gmail.com>
2020-10-14 21:19:24 -0500

    primera version del proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,76 @@
 const btnHamburger = document.getElementById("Button__hamburger");
 const items = document.getElementById("Navbar__items");
 
-btnHamburger.addEventListener("click", (e) => {
+//////
+
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+const image = document.getElementById("heroImage");
+
+let contador = 0;
+
+const image1 = {
+  id: 1,
+  mobile: "../images/mobile-image-hero-1.jpg",
+  desktop: "../images/desktop-image-hero-1.jpg",
+};
+
+const image2 = {
+  id: 2,
+  mobile: "../images/mobile-image-hero-2.jpg",
+  desktop: "../images/desktop-image-hero-2.jpg",
+};
+
+const image3 = {
+  id: 3,
+  mobile: "../images/mobile-image-hero-3.jpg",
+  desktop: "../images/desktop-image-hero-3.jpg",
+};
+
+const images = [image1, image2, image3];
+
+btnHamburger.addEventListener("click", despliegueHamburguesa);
+btnLeft.addEventListener("click", anterior);
+btnRight.addEventListener("click", siguiente);
+
+window.addEventListener("resize", function(){
+  // tu código aquí
+  let n,d
+  n = image.src.length - 5;
+  d = parseInt(image.src.slice(n, -4));
+  console.log(d);
+  render(d);
+});
+
+function despliegueHamburguesa() {
   btnHamburger.classList.toggle("open");
   items.classList.toggle("show");
-});
+}
+
+function anterior() {
+  contador--;
+  if (contador < 0) {
+    contador = images.length -1;
+  }
+  render(contador);
+}
+
+function siguiente() {
+  contador++;
+  if (contador >= images.length) {
+    contador = 0;
+  }
+  render(contador);
+}
+
+function render(n) {
+  console.log(images[n].id)
+  const widhtScreen = screen.width;
+  if(widhtScreen > 414){
+    image.src = images[n].desktop;
+  }
+  if(widhtScreen <= 414){
+    image.src = images[n].mobile
+  }
+}
+
